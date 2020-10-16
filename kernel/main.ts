63cb4a19c75fec1cb5f0d6856a55f1d3d787c6b6
jaensen <no@email.comm>
2020-10-16 03:18:40 +0200

    added the hash to the marketplace bucket

--- kernel/main.ts
+++ kernel/main.ts
@@ -7,7 +7,7 @@ const appHashNameLookup = {
   "bafzbeidz3eazquyorhjdiosdgbc5j73yz5omnyqrasuz7pertimlmz7e5y": "odentity",
   "bafzbeicmtet2ytuo5jlg2jtuh4rbtfvntznwah5mt2kb4xj3zgxt2ol5ma": "wallet",
   "bafzbeiafbjcuy4dxnily3nbt7nab6ebdwyti3z7jgdrblnm4ivqw7hubki": "textile",
-  "": "marketplace"
+  "bafzbeiahddbruy5letgjx6tiijzaednwr3zngtk57u3yyrjcsba7sqjbdq": "marketplace"
 }
 
 export const isLocal = window.location.hostname == "localhost"
