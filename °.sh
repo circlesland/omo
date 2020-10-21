6e0ef0f18db5fad01c07f1ee98c7a177bfb85479
Samuel Andert <samuelandert@me.com>
2020-10-21 21:11:19 +0200

    init script added

---
+++ °.sh
@@ -0,0 +1,55 @@
+if [ -z "$(ls -A °os)" ]; then
+    git clone https://github.com/omoearth/o-os.git °os
+else
+   git fetch °os
+fi
+
+
+
+if [ -z "$(ls -A °circles-protocol)" ]; then
+    git clone https://github.com/omoearth/o-circles-protocols.git °circles-protocol
+else
+   git fetch °circles-protocol
+fi
+
+
+
+if [ -z "$(ls -A °views)" ]; then
+    git clone https://github.com/omoearth/o-views.git °views
+else
+   git fetch °views
+fi
+
+
+
+if [ -z "$(ls -A dapps/°dentity)" ]; then
+    git clone https://github.com/omoearth/o-dapp-odentity.git dapps/°dentity
+else
+    git fetch dapps/°dentity
+fi
+
+
+
+if [ -z "$(ls -A dapps/°market)" ]; then
+    git clone https://github.com/omoearth/o-dapp-market.git dapps/°market
+else
+    git fetch dapps/°market
+fi
+
+
+
+if [ -z "$(ls -A dapps/°wallet)" ]; then
+    git clone https://github.com/omoearth/o-dapp-wallet.git dapps/°wallet
+else
+    git fetch dapps/°wallet
+fi
+
+
+
+if [ -z "$(ls -A services/°ipfs)" ]; then
+    git clone https://github.com/omoearth/o-service-ipfs.git services/°ipfs  
+else
+    git fetch services/°ipfs
+fi
+
+echo "° ready"
